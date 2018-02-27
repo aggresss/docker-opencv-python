da50b2674ef4b3a670dd97b9754a4ad7d6d041c6
aggresss <aggresss@163.com>
2018-02-27 15:39:58 +0800

    20180227B

--- jupyter_config.py
+++ jupyter_config.py
@@ -5,5 +5,5 @@ c.NotebookApp.notebook_dir = u'/root/volume'
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_root = True
 c.NotebookApp.port = 8888
-# default password: aggresss
-c.NotebookApp.password = u'sha1:6f30b171bc56:583d72dea6d9594a6238f27b49de6366bbb652c4'
+# default password: 12345678
+c.NotebookApp.password = u'sha1:d501736a80f9:2bf882737f5ded39b8d1803b0c3ca385325fbfa8'
